30c3c4d5da909f39107c17a3b7887fcb1289fdd3
trifonova_o_e <myowl@list.ru>
2024-10-23 15:44:12 +0300

    new

--- src/server/client/ClientGUI.java
+++ src/server/client/ClientGUI.java
@@ -5,6 +5,10 @@ import server.server.ServerWindow;
 import javax.swing.*;
 
 public class ClientGUI {
+
+    private final JTextArea log = new JTextArea();
+
+    private final JPanel panelTab = new JPanel();
     public ClientGUI(ServerWindow serverWindow) {
 
     }
